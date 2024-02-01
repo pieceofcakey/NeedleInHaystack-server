4b89181caf4d2c97acef36077e3f46d8de6358ca
suhjuho <sspaka0422@gmail.com>
2024-02-01 20:50:09 +0900

    Feat: add utils text Analyzer

---
+++ src/utils/analyzeText.js
@@ -0,0 +1,16 @@
+const stopWords = require("./stopWords");
+
+function analyzeText(text) {
+  text = text.replace(/(?:https?|ftp):\/\/[\w\S]+/g, ""); // Remove URLs
+  text = text.replace(/[^a-zA-Z\s]/g, ""); // Remove symbols, punctuation, number
+  text = text.replace(/\s+/g, " "); // Remove extra whitespace
+  text = text.toLowerCase(); // Lowercase text
+
+  const tokens = text
+    .split(" ")
+    .filter((word) => !stopWords.includes(word.toLowerCase()));
+
+  return tokens;
+}
+
+module.exports = { analyzeText };
