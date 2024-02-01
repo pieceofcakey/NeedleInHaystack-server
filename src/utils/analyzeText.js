e4ea8c9ec574e240586e0e83eacaad516f5d973a
suhjuho <sspaka0422@gmail.com>
2024-02-01 22:26:43 +0900

    Style: erase  annotation

--- src/utils/analyzeText.js
+++ src/utils/analyzeText.js
@@ -1,14 +1,14 @@
-const { stopWords } = require("./textData");
+const { STOP_WORDS } = require("../constants/wordData");
 
 function analyzeText(text) {
-  text = text.replace(/(?:https?|ftp):\/\/[\w\S]+/g, ""); // Remove URLs
-  text = text.replace(/[^a-zA-Z\s]/g, ""); // Remove symbols, punctuation, number
-  text = text.replace(/\s+/g, " "); // Remove extra whitespace
-  text = text.toLowerCase(); // Lowercase text
+  text = text.replace(/(?:https?|ftp):\/\/[\w\S]+/g, "");
+  text = text.replace(/[^a-zA-Z\s]/g, "");
+  text = text.replace(/\s+/g, " ");
+  text = text.toLowerCase();
 
   const tokens = text
     .split(" ")
-    .filter((word) => !stopWords.includes(word.toLowerCase()));
+    .filter((word) => !STOP_WORDS.includes(word.toLowerCase()));
 
   return tokens;
 }
