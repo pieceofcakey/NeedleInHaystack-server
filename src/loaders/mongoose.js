9371d98392718f0ca1e4e4e5c22be37d572577a5
seunggyu <devkim0702@gmail.com>
2024-02-02 21:37:34 +0900

    Style: add new line (eslint)

--- src/loaders/mongoose.js
+++ src/loaders/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function mongooseLoader() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, { dbName: "pieceoftube" });
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "pieceoftube",
+    });
     console.log("connected to database");
   } catch (error) {
     console.error(error);
