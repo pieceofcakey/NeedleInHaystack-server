8d9c729277c2dab76f4dda6017ed3a4900019ff4
seunggyu <devkim0702@gmail.com>
2024-02-07 18:05:32 +0900

    Style: change response type of /auto-completions API from an array to object for uniformed code style

--- src/controllers/autoCompletion.controller.js
+++ src/controllers/autoCompletion.controller.js
@@ -6,7 +6,10 @@ exports.getAutoCompletions = async function (req, res, next) {
   const MAXIMUM_AUTO_COMPLETIONS = 5;
 
   if (!userInput) {
-    return res.status(200).send([]);
+    return res.status(200).send({
+      result: "ok",
+      searchHistories: [],
+    });
   }
 
   try {
@@ -21,7 +24,10 @@ exports.getAutoCompletions = async function (req, res, next) {
       .slice(0, MAXIMUM_AUTO_COMPLETIONS)
       .forEach((element) => searchHistories.push(element.text));
 
-    res.status(200).send(searchHistories);
+    res.status(200).send({
+      result: "ok",
+      searchHistories,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
