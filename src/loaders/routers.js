55eefdacf504309260aa9b48b4213d8ec5beebbb
seunggyu <devkim0702@gmail.com>
2024-02-09 20:32:26 +0900

    Feat: set routers for signIn and signOut

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -2,12 +2,16 @@ const indexRouter = require("../routes/index");
 const keywordsRouter = require("../routes/keywords");
 const autoCompletionsRouter = require("../routes/autoCompletions");
 const videosRouter = require("../routes/videos");
+const signInRouter = require("../routes/signIn");
+const signOutRouter = require("../routes/signOut");
 
 async function routerLoader(app) {
   app.use("/", indexRouter);
   app.use("/keywords", keywordsRouter);
   app.use("/auto-completions", autoCompletionsRouter);
   app.use("/videos", videosRouter);
+  app.use("/signIn", signInRouter);
+  app.use("/signOut", signOutRouter);
 }
 
 module.exports = routerLoader;
