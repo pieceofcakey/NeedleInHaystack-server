4b23063b92413654964719178507cfef89a193a7
seunggyu <devkim0702@gmail.com>
2024-02-09 20:33:12 +0900

    Feat: complete signIn and signOut controllers

---
+++ src/controllers/signOut.controller.js
@@ -0,0 +1,6 @@
+exports.signOut = async function (req, res, next) {
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+
+  res.send({ result: "ok", message: "logout! successfully" });
+};
