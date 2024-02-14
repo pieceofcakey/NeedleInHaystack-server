5bdcdb22da4f6f8f9950cf60bf0d187c1d6be249
suhjuho <sspaka0422@gmail.com>
2024-02-15 03:55:37 +0900

    Feat: add combine bm25, rankpage

---
+++ src/crawler/rank.js
@@ -0,0 +1,28 @@
+const Keyword = require("../models/Keyword");
+
+async function rank() {
+  console.log("Start Ranking for all keywords");
+
+  const keywords = await Keyword.find().populate("videos.videoId");
+
+  try {
+    await Promise.all(
+      keywords.map(async (keyword) => {
+        keyword.videos.forEach((video) => {
+          video.score *= parseFloat(video.videoId.pageRankScore, 10);
+        });
+
+        keyword.videos = keyword.videos.sort(
+          (videoOne, videoTwo) => videoTwo.score - videoOne.score,
+        );
+
+        await keyword.save();
+      }),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+  console.log("Finish Ranking for all keywords");
+}
+
+module.exports = { rank };
