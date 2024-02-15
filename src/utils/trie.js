a782ad0a8394e42a350cf278b23492686b140e54
seunggyu <devkim0702@gmail.com>
2024-02-15 22:58:46 +0900

    Feat: implement Trie data structure for improved spelling check feature

---
+++ src/utils/trie.js
@@ -0,0 +1,39 @@
+const TrieNode = require("./trieNode");
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let node = this.root;
+
+    word.split(" ").forEach((char) => {
+      if (!node.children.has(char)) {
+        node.children.set(char, new TrieNode());
+      }
+
+      node = node.children.get(char);
+    });
+
+    node.isEndOfWord = true;
+  }
+
+  search(word) {
+    let node = this.root;
+
+    return (
+      word.split("").every((char) => {
+        if (!node.children.has(char)) {
+          return false;
+        }
+
+        node = node.children.get(char);
+
+        return true;
+      }) && node.isEndOfWord
+    );
+  }
+}
+
+module.exports = Trie;
