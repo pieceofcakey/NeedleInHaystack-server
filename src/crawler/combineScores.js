a2e9998b423aa2d51d7422079f8b8b261e0a8ecd
suhjuho <sspaka0422@gmail.com>
2024-02-18 20:57:58 +0900

    Feat: add combineScores

--- src/crawler/combineScores.js
+++ src/crawler/combineScores.js
@@ -1,6 +1,11 @@
-const Keyword = require("../models/Keyword");
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../../.env") });
+const mongooseLoader = require("../loaders/mongoose");
 
-async function rank() {
+const Keyword = require("../models/Keyword");
+const Video = require("../models/Video");
+
+async function combinePageRankAndBM25() {
   console.log("Start Ranking for all keywords");
 
   const keywords = await Keyword.find().populate("videos.videoId");
@@ -10,13 +15,15 @@ async function rank() {
       keywords.map(async (keyword) => {
         keyword.videos.forEach((video) => {
           video.score =
-            video.scoreBM * (1 + parseFloat(video.videoId.pageRankScore, 10));
+            parseFloat(video.scoreBM, 10) *
+            (1 + parseFloat(video.videoId.pageRankScore, 10));
         });
 
         keyword.videos = keyword.videos.sort(
           (videoOne, videoTwo) => videoTwo.score - videoOne.score,
         );
 
+        console.log(keyword.text);
         await keyword.save();
       }),
     );
@@ -26,4 +33,11 @@ async function rank() {
   console.log("Finish Ranking for all keywords");
 }
 
-module.exports = { rank };
+async function combineScores() {
+  await mongooseLoader();
+  const start = Date.now();
+  await combinePageRankAndBM25();
+  console.log(Date.now() - start);
+}
+
+combineScores();
